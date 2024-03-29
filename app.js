218cfbe0a738ae7c5368b2a598c1561722573a8c
Sviatoslav <sviatos1706@gmail.com>
2024-03-29 20:19:02 +0200

    connected to mongoDB

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express =require('express');
+const mongoose=require('mongoose');
 
 const PORT=3000;
 
 const app=express();
 
 app.use(express.static('public'));
+
+mongoose.connect(`mongodb+srv://sviatos1706:xbX2nEdUiIdjZ8Dx@cluster0.lht4zaw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+.then(()=>{
+    console.log('connected to mongoDB')
+})
 
 app.listen(PORT,()=>{
     console.log(`Server work on port:${PORT}`)
