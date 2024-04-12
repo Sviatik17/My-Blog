6833320563fa06864b68909e5130eccac679e7f3
Sviatoslav <sviatos1706@gmail.com>
2024-04-12 18:43:32 +0300

    add creating and displaying posts

--- app.js
+++ app.js
@@ -29,6 +29,16 @@ mongoose.connect(`mongodb+srv://root:jbjhg76t476f@cluster0.ebopdt7.mongodb.net/?
             res.status(500).json({message: err.message})
         }
     })
+
+    app.get('/posts', async(req,res)=>{
+        try{
+            const posts= await Post.find();
+            res.json(posts);
+        }catch{
+            res.status(500).json({message:err.message})
+        } 
+      
+    })
     
     app.get('/create-post',(req,res)=>{
         res.sendFile(path.join(__dirname,'public','createPost.html'))
@@ -39,69 +49,4 @@ app.listen(PORT,()=>{
 })
 
 
-
-
-
-
-
-
-// const { MongoClient } = require("mongodb");
-
-// // Connection URI
-// const uri =
-//   "mongodb+srv://sviatos1706:xbX2nEdUiIdjZ8Dx@http://localhost:3000//?poolSize=20&writeConcern=majority";
-
-// // Create a new MongoClient
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server
-//     await client.connect();
-
-//     // Establish and verify connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Connected successfully to server");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-
-
-
-
-
-
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://sviatos1706:xbX2nEdUiIdjZ8Dx@cluster0.lht4zaw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
 
--- public/main.js
+++ public/main.js
@@ -10,3 +10,23 @@ $('.createPost').click(function(){
     location.href='/'
 })
 })
+
+function getPosts(){
+    
+        axios.get('http://localhost:3000/posts')
+        .then((res)=>{
+            console.log(res.data)
+
+            for(let el of res.data){
+                $('.postsArea').append(`
+                <div class="postCard">
+                <div class="title">${el.title}</div>
+                <div class="content">${el.content}</div>
+                <div class="author">${el.author}</div>
+                </div>
+                `)
+            }
+        })
+        
+}
+getPosts()
